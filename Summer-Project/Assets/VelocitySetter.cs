f97eec4158e4d5717c4164a55aa0802672ac2791
Justin Fernald <jfernald@my.gcu.edu>
2019-05-26 09:33:07 -0700

    Baseline testing

---
+++ Summer-Project/Assets/VelocitySetter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocitySetter : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddRelativeForce(velocity);
+    }
+}
